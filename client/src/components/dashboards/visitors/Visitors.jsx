461d4748233e7b4c3735195bf5e084dc8868962d
Kavivarman-Developer <kavivarman6722@gmail.com>
2026-04-12 22:18:21 +0530

    fix cors and axios headers

--- client/src/components/dashboards/visitors/Visitors.jsx
+++ client/src/components/dashboards/visitors/Visitors.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 const API = import.meta.env.VITE_API_URL; // ✅ backend URL
+axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
 
 function initials(name) {
     return name.split(" ").map((w) => w[0]).join("").slice(0, 2).toUpperCase();
